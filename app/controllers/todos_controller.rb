cbed887cc63f5aa2de4160a7492580cf61823dff
Kellen Vanetti <kellen@kellenvanetti.net>
2017-08-30 05:40:40 +0000

    Add delete action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -34,7 +34,13 @@ class TodosController < ApplicationController
  
  def index
    @todos = Todo.all
-   
+  end
+ 
+ def destroy
+   @todo = Todo.find(params[:id])
+   @todo.destroy 
+   flash[:notice] = "Todo was destroyed successfully"
+   redirect_to todos_path
  end
  
  private
